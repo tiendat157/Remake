9ccd07c702fa6edb89ed5f545cfa6a2f055a73ee
NguyenDuyTruongGiang <Bizuma123@gmail.com>
2023-05-27 06:58:56 +0700

    Admin Site

---
+++ Controllers/SpinController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceCode.Service;
+using SourceCode.Models;
+
+namespace SourceCode.Controllers
+{
+    public class SpinController : Controller
+    {
+        private UsersSpinService _usersSpinService;
+        public SpinController(UsersSpinService usersSpinService)
+        {
+            _usersSpinService = usersSpinService;
+        }
+        // GET: Spin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetNumberOfSpin()
+        {
+            Models.User user = Session["User"] as Models.User;
+            if (user == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            int result = _usersSpinService.GetNumberOfSpinsByUserID(user.ID);
+            return Json(new { num = result }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
